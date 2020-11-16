ce7bd1ecb4381821f832c414d2aa26ebc260e03c
Redpenguine <36995050+Redpenguine@users.noreply.github.com>
2020-11-16 14:07:31 +0200

    change

---
+++ TowerDefenseGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    bool paused = false;
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!paused)
+            {
+                Time.timeScale = 0;
+                paused = true;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                TimeGo();
+            }
+        }
+    }
+    public void ExitToMenu()
+    {
+        TimeGo();
+        SceneManager.LoadScene("Menu");
+    }
+    public void Continue()
+    {
+        TimeGo();
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void TimeGo()
+    {
+        Time.timeScale = 1;
+        paused = false;
+        pauseMenu.SetActive(false);
+    }
+}
---
+++ TowerDefenseGame/Assets/Scripts/TurretObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Turret", menuName = "Card")]
+public class TurretObject : ScriptableObject
+{
+    public new string name;
+    public int damage;
+    
+}
